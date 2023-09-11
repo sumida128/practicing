d3f8b2638a117f7fa53931eba2c8c7b656f1aad6
honestInfo <honest2021info@gmail.com>
2023-09-11 18:19:40 +0900

    try01

--- src/App.java
+++ src/App.java
@@ -4,7 +4,19 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("To start practice. ");
 
+        if (args.length == 0) {
+
+            args = new String[]{"",""};
+        }
+
+        /*
+         * Offer 05.
+         * 
+         * Ex.1 
+         * input : s = "We are happy."
+         * output : "We%20are%20happy." 
+         */
         Practice001_05 practice001_05 = new Practice001_05();
-        practice001_05.try01(args);
+        practice001_05.try01(new String[]{"We are happy."});
     }
 }
--- src/sp/Practice001_05.java
+++ src/sp/Practice001_05.java
@@ -3,5 +3,6 @@ package sp;
 public class Practice001_05 {
     public void try01(String[] args) throws Exception {
         System.out.println("Run try01. ");
+        System.out.println(args.toString());
     }
 }
