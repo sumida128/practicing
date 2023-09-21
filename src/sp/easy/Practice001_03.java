3c6121cff01062d03f1dd9bd466cf382e98c59a2
honestInfo <honest2021info@gmail.com>
2023-09-21 15:51:44 +0900

    To practice 03 summarize

--- src/sp/easy/Practice001_03.java
+++ src/sp/easy/Practice001_03.java
@@ -1,6 +1,7 @@
 package sp.easy;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 /*
@@ -19,13 +20,19 @@ import java.util.HashMap;
 public class Practice001_03 {
 
     // グローバル変数 global variable
-    private ArrayList<String> headListGlobal = new ArrayList<>();
 
+    // コンストラクタ
     public Practice001_03() {
         System.out.println("Run Practice001_03. ");
 
     }
 
+    /**
+     * 事前処理、パラメータをプリント
+     * 
+     * @param param_1 int[]
+     * @param param_2 String メソッド名
+     */
     public void runBefore(int[] param_1, String param_2) {
 
 		System.out.println("Run Practice001_03." + param_2);
@@ -38,16 +45,13 @@ public class Practice001_03 {
         }
         System.out.print("\r\n");
 
-		System.out.print("▲ param_2 = ");
-        // for(int i = 0; i < param_2.length; i++) {
-
-            System.out.print(param_2 + ",");
-        // }
+		System.out.print("▲ param_2 = " + param_2 + ",");
         System.out.println("  \u25B2  ");
 	}
 
     /*
      * ArrayList<String>.remove を利用する
+     * デメリット：二つループを使った
      */
     public void findRepeatNumber_try01(int[] args) throws Exception {
         runBefore(args, "findRepeatNumber_try01");
@@ -75,16 +79,94 @@ public class Practice001_03 {
             k = 0;
         }
 
-        
         System.out.println("実行後：" + "重複値＝回数" );
         System.out.println(findedInt);
-
-
-        // System.out.println("実行後");
-        // for(int i = 0; i < headListGlobal.size(); i++) {
-
-        //     System.out.print(headListGlobal.get(i) + ",");
-        // }
     }
-    
+
+    /*
+     * 交換と順番を利用する
+     * 
+     */
+    public void findRepeatNumber_try02(int[] args) throws Exception {
+        runBefore(args, "findRepeatNumber_try02");
+
+        int resoult = -1;
+        int i = 0;
+        while(i < args.length) {
+            
+            // 解法有一个关键点是只有 nums[i] == i 的时候i才递增，
+            // 这样保证找到相同元素前不会漏掉某些元素的处理
+            //値の「0」は配列コンテナ[0]に置かないといけません。
+            //値の「1」は配列コンテナ[1]に置かないといけません。
+            if(args[i] == i) {
+                i++;
+                continue;
+            }
+
+            int temp = args[i];
+            int target = args[temp];
+
+            // 同じアイテムがあれば
+            if(temp == target) {
+
+                // その値を返却
+                resoult = target; 
+                // 重複値を探した
+                break;
+            } else {
+
+                //値の数字と配列コンテナの位置順番は同じまで
+                //値を交換して次のチェックを行う
+                args[temp] = temp;
+                args[i] = target;
+            }
+
+        }
+
+        //先会ったの重複値を返す
+        System.out.println("実行後：" + "重複値＝回数" );
+        System.out.println(resoult);
+    }
+
+    /*
+     * 順番にして重複値を探す
+     */
+    public void findRepeatNumber_try03(int[] args) throws Exception {
+        runBefore(args, "findRepeatNumber_try03");
+
+        //順番にする
+        Arrays.sort(args);
+ 
+        Integer[] resoult = new Integer[args.length];
+        int index = 0;
+        
+        String[] resoultS = new String[]{"A","B"};
+ 
+        //順番にしてから重複値がある場合、連続している
+        //順番「1」から
+        for (int i = 1; i < args.length; i++) {
+
+            //配列[0]と[1]一個前の値比較...
+            if (args[i] == args[i - 1]){
+
+                //重複値を探したら追加
+                resoult[index++] = args[i];
+             }
+        }
+
+        System.out.println("実行後：" + "重複値＝回数" );
+        // 配列から ArrayListに変換
+        ArrayList<Integer> arrayListInt = new ArrayList<Integer>(Arrays.asList(resoult));
+        // 配列から ArrayListに変換
+        ArrayList<String> arrayListStr = new ArrayList<String>(Arrays.asList(resoultS));
+        
+        // 配列プリントできない 1[Ljava.lang.Integer;@548c4f57
+        System.out.println("1" + resoult);
+        // ArrayListプリントできる
+        System.out.println("2" + arrayListInt);
+        // ArrayListプリントできる
+        System.out.println("3" + arrayListStr);
+        // 配列プリントしたい場合
+        System.out.println("4" + Arrays.toString(resoult));
+    }
 }
