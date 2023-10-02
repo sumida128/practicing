b584023178f114989beee5b231f474e7fb3973ae
sumida128 <sumida128@gmail.com>
2023-10-02 11:25:52 +0900

    Leet Code 104. commit update2

--- src/App.java
+++ src/App.java
@@ -31,7 +31,7 @@ public class App {
         
         /**
          * Binary Tree-DFS
-         * 104. Maximum Depth of Binary Tree. 
+         * Leet Code 104. Maximum Depth of Binary Tree. 
          * Given the root of a binary tree, return its maximum depth.
          * A binary tree's maximum depth is the number of nodes along the longest path from the
          * root node down to the farthest leaf node.
