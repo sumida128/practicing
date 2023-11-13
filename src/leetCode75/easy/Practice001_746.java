90b753ec665c27d10507112a47fbe5fee22e931a
honestInfo <honest2021info@gmail.com>
2023-11-13 14:18:33 +0900

    LeetCode75.746.MinCost Climbing Stairs不要ソースコメントアウト

--- src/leetCode75/easy/Practice001_746.java
+++ src/leetCode75/easy/Practice001_746.java
@@ -177,132 +177,132 @@ public class Practice001_746 {
         // return minCost;
     }
 
-    /**
-     * 
-     * @param 範囲 n, 予想値 pick
-     * @return
-     */
-    public int guessNumber_374_try01(int n, int pick) {
+    // /**
+    //  * 
+    //  * @param 範囲 n, 予想値 pick
+    //  * @return
+    //  */
+    // public int guessNumber_374_try01(int n, int pick) {
 
-        // 両端を定義
-        int left = 1;
-        int rigth = n;
-        int count = 1;
+    //     // 両端を定義
+    //     int left = 1;
+    //     int rigth = n;
+    //     int count = 1;
 
-        while (left <= rigth ) {
-            // 二分法
-            int mid = left + (( rigth - left) / 2); 
-            // Forward declaration of guess API.获取猜测结果
-            int result = guess(mid, pick);
+    //     while (left <= rigth ) {
+    //         // 二分法
+    //         int mid = left + (( rigth - left) / 2); 
+    //         // Forward declaration of guess API.获取猜测结果
+    //         int result = guess(mid, pick);
 
-            if (result == 0) {
-                break ;
-            }
-            if (result == 1) {
-                left = mid + 1;
-            }
-            if (result == -1) {
-                rigth = mid - 1;
-            }
+    //         if (result == 0) {
+    //             break ;
+    //         }
+    //         if (result == 1) {
+    //             left = mid + 1;
+    //         }
+    //         if (result == -1) {
+    //             rigth = mid - 1;
+    //         }
 
-            count++;
-        }
-        System.out.println("実行回数：" + count);
+    //         count++;
+    //     }
+    //     System.out.println("実行回数：" + count);
 
-        return pick ;
-    }
+    //     return pick ;
+    // }
 
-    public int guess(int mid ,int pick) {
+    // public int guess(int mid ,int pick) {
 
-        if(pick > mid) {
-            return 1;
-        } 
-        else if(pick < mid) {
-            return -1;
-        }
-        else if(pick ==  mid) {
-            return 0;
-        }
-        return 9;
-    }
+    //     if(pick > mid) {
+    //         return 1;
+    //     } 
+    //     else if(pick < mid) {
+    //         return -1;
+    //     }
+    //     else if(pick ==  mid) {
+    //         return 0;
+    //     }
+    //     return 9;
+    // }
 
-    /**
-     * Binary Search Tree
-     * 特別なところ：左枝と右枝
-     * 左枝：必ず親枝によって小
-     * 右枝：必ず親枝によって大きい
-     * 
-     * @param matrix
-     * @return
-     * @throws Exception
-     */
-    public TreeNode searchBST_700_try01(TreeNode root, int val) {
+    // /**
+    //  * Binary Search Tree
+    //  * 特別なところ：左枝と右枝
+    //  * 左枝：必ず親枝によって小
+    //  * 右枝：必ず親枝によって大きい
+    //  * 
+    //  * @param matrix
+    //  * @return
+    //  * @throws Exception
+    //  */
+    // public TreeNode searchBST_700_try01(TreeNode root, int val) {
 
-        // 入力確認、最後leafなら子供がないので、「null」を返却
-        if (null == root || root.isLeaf()) {
-            return null;
-        }
+    //     // 入力確認、最後leafなら子供がないので、「null」を返却
+    //     if (null == root || root.isLeaf()) {
+    //         return null;
+    //     }
 
-        // TreeNodeから値を文字化して枝番を取得する
-        String rootStr =root.toString().substring(root.toString().length() -1);
-        // 文字の枝番を取得して数字化に転換する
-        int rootInt  = Integer.parseInt(rootStr);
+    //     // TreeNodeから値を文字化して枝番を取得する
+    //     String rootStr =root.toString().substring(root.toString().length() -1);
+    //     // 文字の枝番を取得して数字化に転換する
+    //     int rootInt  = Integer.parseInt(rootStr);
 
-        //余計なソース
-        // TreeNodeの子供を取得
-        TreeNode leftBranch = root.getChildAt(0);
-        // TreeNodeから値を文字化して枝番を取得する
-        String leftBranchStr =leftBranch.toString().substring(leftBranch.toString().length() -1);
-        // 文字の枝番を取得して数字化に転換する
-        int leftBranchInt  = Integer.parseInt(leftBranchStr);
+    //     //余計なソース
+    //     // TreeNodeの子供を取得
+    //     TreeNode leftBranch = root.getChildAt(0);
+    //     // TreeNodeから値を文字化して枝番を取得する
+    //     String leftBranchStr =leftBranch.toString().substring(leftBranch.toString().length() -1);
+    //     // 文字の枝番を取得して数字化に転換する
+    //     int leftBranchInt  = Integer.parseInt(leftBranchStr);
         
-        // TreeNodeの子供を取得
-        TreeNode rightBranch = root.getChildAt(1);
-        // TreeNodeから値を文字化して枝番を取得する
-        String rightBranchStr =rightBranch.toString().substring(rightBranch.toString().length() -1);
-        // 文字の枝番を取得して数字化に転換する
-        int rightBranchInt  = Integer.parseInt(rightBranchStr);
-        //余計なソース
+    //     // TreeNodeの子供を取得
+    //     TreeNode rightBranch = root.getChildAt(1);
+    //     // TreeNodeから値を文字化して枝番を取得する
+    //     String rightBranchStr =rightBranch.toString().substring(rightBranch.toString().length() -1);
+    //     // 文字の枝番を取得して数字化に転換する
+    //     int rightBranchInt  = Integer.parseInt(rightBranchStr);
+    //     //余計なソース
 
-        //今のブランチを確認
-        System.out.println("今のブランチ" + root.toString());
-        if (rootInt >  val) {
-            return searchBST_700_try01(leftBranch, val);
-        }
+    //     //今のブランチを確認
+    //     System.out.println("今のブランチ" + root.toString());
+    //     if (rootInt >  val) {
+    //         return searchBST_700_try01(leftBranch, val);
+    //     }
 
-        if (rootInt < val) {
-            return searchBST_700_try01(rightBranch, val);
-        }
+    //     if (rootInt < val) {
+    //         return searchBST_700_try01(rightBranch, val);
+    //     }
 
-        // rootInt == val の場合
-        return root ;
-    }
+    //     // rootInt == val の場合
+    //     return root ;
+    // }
 
-    /**
-     * Recursionを使う
-     * 
-     * @param matrix
-     * @return
-     * @throws Exception
-     */
-    public int maxDepth_104_try02(TreeNode root ) throws Exception {
+    // /**
+    //  * Recursionを使う
+    //  * 
+    //  * @param matrix
+    //  * @return
+    //  * @throws Exception
+    //  */
+    // public int maxDepth_104_try02(TreeNode root ) throws Exception {
 
-        // 入力確認、最後leafなら、「1」を返却
-        if (null == root) {
-            return 0;
-        } 
+    //     // 入力確認、最後leafなら、「1」を返却
+    //     if (null == root) {
+    //         return 0;
+    //     } 
         
-        //今のブランチを確認
-        System.out.println("今のブランチ" + root.toString());
-        if (root.isLeaf()) {
-            return 1;
-        }
+    //     //今のブランチを確認
+    //     System.out.println("今のブランチ" + root.toString());
+    //     if (root.isLeaf()) {
+    //         return 1;
+    //     }
 
-        // 大きい方を取得
-        return Math.max(
-            maxDepth_104_try02(
-                root.getChildAt(0)), 
-            maxDepth_104_try02(
-                root.getChildAt(1))) + 1;
-    }
+    //     // 大きい方を取得
+    //     return Math.max(
+    //         maxDepth_104_try02(
+    //             root.getChildAt(0)), 
+    //         maxDepth_104_try02(
+    //             root.getChildAt(1))) + 1;
+    // }
 }
